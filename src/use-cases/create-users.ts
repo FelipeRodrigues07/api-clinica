698f7c29f5c2cebdd4f8589de802c1d800d0dbdf
FelipeRodrigues07 <feliperodriguesrs07@outlook.com>
2024-08-07 16:39:04 -0300

    feat: add create style

---
+++ src/use-cases/create-users.ts
@@ -0,0 +1,43 @@
+import { UsersRepository } from 'src/repositories/users-repository'
+import { hash } from 'bcryptjs'
+import { UsersAlreadyExistsError } from 'src/errors/user-already-exists-error'
+import { User } from '@prisma/client'  //tipagem propria do prisma
+
+interface CreateAccountUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+interface CreateAccountUseCaseResponse {
+  user: User
+}
+
+
+
+export class RegisterUseCase {  
+  constructor(private usersRepository: UsersRepository) {}   
+
+  async execute({ name, email, password }: CreateAccountUseCaseRequest): Promise<CreateAccountUseCaseResponse> {
+    const password_hash = await hash(password, 6)
+
+   
+    const userWithSameEmail = await this.usersRepository.findByEmail(email) 
+  
+
+    if (userWithSameEmail) { 
+      throw new UsersAlreadyExistsError()
+    }
+
+                     
+    const user = await this.usersRepository.create({   
+      name,
+      email,
+      password_hash,
+    })
+
+    return {
+      user
+    }
+  }
+}
